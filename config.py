d3d88cd7a5f25cd010c7febedd253d267981c8ef
Shashank Comandur <shashank.comandur@gmail.com>
2026-04-13 19:23:44 -0400

    feat: ui v1

---
+++ config.py
@@ -0,0 +1,20 @@
+PITCH_TYPES = [
+    '4-Seam Fastball',
+    'Slider',
+    'Changeup',
+    'Curveball',
+    'Cutter',
+    'Sinker',
+    'Splitter',
+    'Knuckle Curve',
+    'Sweeper'
+]
+
+STRIKE_ZONE = {
+    'left': -0.71,
+    'right': 0.71,
+    'bottom': 1.5,
+    'top': 3.5
+}
+MOVEMENT_SCALE = 0.4
+MOVEMENT_THRESHOLD = 0.5
